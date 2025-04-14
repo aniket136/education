64d81b4266f72df4b754158934760c36592a1336
Aniket8929 <aniketkumarrai271@gmail.com>
2025-04-14 15:13:15 +0530

    Initial commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,7 @@
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/education/',  // 👈 this MUST match your GitHub repo name
   plugins: [react()],
-  base: '/education/', // 🔥 must match your GitHub repo name
 })
-
