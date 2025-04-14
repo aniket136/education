4fbc3012600fb0da93926127c7faa8ab4f157ee6
Aniket <jerryaniket888@gmail.com>
2025-04-14 17:12:51 +0530

    Update Contact.jsx

--- frontend/src/components/Contact.jsx
+++ frontend/src/components/Contact.jsx
@@ -42,7 +42,7 @@ const Contact = () => {
 
     setLoading(true);
     try {
-      const response = await axios.post("https://ngo-e9k0.onrender.com/api/contact/detail", formData);
+      const response = await axios.post("https://education-backend-1.onrender.com/api/contact/detail", formData);
       if (response.status === 201) {
         setShowToast(true);
         setFormData({ name: "", email: "", phone: "", message: "" });
